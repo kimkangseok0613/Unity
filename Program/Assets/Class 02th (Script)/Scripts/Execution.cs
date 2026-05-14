9d02df17e14f270d1c86bf830151c54afa75ad70
kimkangseok0613 <ks0006131@gmail.com>
2026-05-14 21:43:49 +0900

    (2026.05.14) 스크립트

---
+++ Program/Assets/Class 02th (Script)/Scripts/Execution.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Execution : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Awake 함수
+        // 게임 오브젝트가 생성되었을 때 단 한 번만 호출되며,
+        // 스크립트가 비활성화되어있어도 호출되는 이벤트 함수입니다.
+
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        // OnEnable 함수
+        // 게임 오브젝트가 활성화되었을 때 호출되는 이벤트 함수입니다.
+
+        Debug.Log("OnEnable");
+    }
+
+    private void Start()
+    {
+        // Start 함수
+        // 게임 오브젝트가 생성되었을 때 호출되는 이벤트 함수입니다.
+
+        Debug.Log("Start");
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdate 함수
+        // 물리 시스템에 설정된 값에 따라 일정한 간격으로 호출되는 이벤트 함수입니다.
+
+        Debug.Log("Fixed Update");
+    }
+
+    private void Update()
+    {
+        // Update 함수
+        // 매 프레임마다 호출되는 이벤트 함수입니다.
+
+        Debug.Log("Update");
+    }
+
+    private void LateUpdate()
+    {
+        // LateUpdate 함수
+        // Update 함수가 끝나고 난 후에 호출되는 이벤트 함수입니다.
+
+        Debug.Log("Late Update");
+    }
+}
