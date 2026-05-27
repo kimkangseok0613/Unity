40fe4caa13ecad4af4ea0817414ebb58fae44e70
kimkangseok0613 <ks0006131@gmail.com>
2026-05-27 21:43:40 +0900

    (2026.05.27) 프리펩 시스템

--- Program/Assets/Class 06th (Input System)/Scripts/Character.cs
+++ Program/Assets/Class 06th (Input System)/Scripts/Character.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public Vector3 direction;
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
 
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.z = Input.GetAxis("Vertical");
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
 
-        transform.position = transform.position + direction;
+        // Time.deltaTime
+        // 이전 프레임에서 현재 프레임까지 걸린 시간입니다.
+
+        direction.Normalize();
+
+        transform.position = transform.position + direction * speed * Time.deltaTime;
     }
 }
