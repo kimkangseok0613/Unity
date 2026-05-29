311586b924bac0354fb9455b06080b755ccb41a0
kimkangseok0613 <ks0006131@gmail.com>
2026-05-29 21:43:36 +0900

    (2026.05.29) 소리 시스템

--- Program/Assets/Class 08th(Physics System)/Scripts/Controller.cs
+++ Program/Assets/Class 08th(Physics System)/Scripts/Controller.cs
@@ -2,13 +2,39 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
+    [SerializeField] Rigidbody Rigidbody;
     void Start()
     {
-        
+        Rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+    }
+    private void FixedUpdate()
+    {
+        // ForceMode.Force(지속적인 힘)
+        // 입력한 힘(F)을 물체의 무게(m)로 나눈 값에 물리 프레임 시간(t)을 곱한 만큼
+        // 속도가 점진적으로 증가합니다.
+        //
+        // ForceMode.Acceleration(지속적인 가속도)
+        // 물체의 무게(m)를 연산에서 제외하며, 입력한 값 자체를 가속도로 지정하여,
+        // 매 프레임마다 시간(t)을 곱한 만큼의 속도로 증가합니다.
+        //
+        // ForceMode.Impulse(순간적인 힘)
+        // 단 한 번의 프레임 만에 입력한 출력량(I)을 무게(m)로 나눈 만큼 속도를
+        // 즉시 변화시키는 힘입니다.
+        //
+        // ForceMode.VelocityChange(순간적인 속도 변화)
+        // 무게(m)와 시간(t)을 모두 무시하며, 입력한 벡터 값 자체가 객체의 다음
+        // 프레임 속도 변화량이 됩니다.
+
+        Rigidbody.AddForce(direction * speed, ForceMode.Force);
     }
 }
---
+++ Program/Assets/Class 08th(Physics System)/Scripts/Sensor.cs
@@ -0,0 +1,19 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {       
+        Debug.Log(other.transform.gameObject.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+    }
+}
