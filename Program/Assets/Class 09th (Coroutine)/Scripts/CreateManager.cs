04b90d8a92a0b478beb5863cbdf78a5f71aaed9f
kimkangseok0613 <ks0006131@gmail.com>
2026-06-03 21:44:22 +0900

    (2026.06.03) 코루틴

--- Program/Assets/Class 09th (Coroutine)/Scripts/CreateManager.cs
+++ Program/Assets/Class 09th (Coroutine)/Scripts/CreateManager.cs
@@ -1,9 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class CreateManager : MonoBehaviour
 {
     [SerializeField] GameObject elemental;
     [SerializeField] float offset = 1.5f;
+    [SerializeField] List<GameObject> list;
+    [SerializeField] int count;
+    [SerializeField] float time;
+
     void Start()
     {
         for (int i=0; i<5; i++)
@@ -11,7 +18,20 @@ public class CreateManager : MonoBehaviour
             GameObject clone = Instantiate(elemental, transform);
 
             clone.transform.position = new Vector3(-3 + (offset * i), 0, 0);
+
+            clone.SetActive(false);
+
+            list.Add(clone);
         }
-        
+        StartCoroutine(Coroutine());
+    }
+    
+    IEnumerator Coroutine()
+    {
+        Debug.Log("Start Coroutine");
+
+        yield return new WaitForSeconds(5f);
+
+        Debug.Log("Stop Coroutine");
     }
 }
